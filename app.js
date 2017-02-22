17e13b11617f27361c89f4143847353f8353bc65
Tina Ko <tinarchan@gmail.com>
2017-02-22 12:19:25 -0800

    Create and render nested React components

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// TODO
+/* CREATE A COMPONENT
+
+In its most basic form, to create a React component you just write a function 
+(with a capitalized name by convention) that returns JSX. The JSX should represent 
+the intended HTML of the rendered component.
+
+*/
+
+var GroceryList = () => (
+  <ul>
+  	<Kiwis />
+  	<Honeydew />
+  </ul>
+);
+
+var Kiwis = () => (
+  <li>Kiwis</li>
+);
+
+var Honeydew = () => (
+  <li>Honeydew</li>
+);
+
+/* 
+To render a React component you use ReactDOM.render(componentInstance, DOMElement). 
+JSX returns a component instance when you wrap a component in an HTML tag. 
+
+*/
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
